a529929cd8138b1fcd83355ef730a3266ee7ec68
lvweicong <316421657@qq.com>
2018-03-01 15:03:23 +0800

    Initial commit

--- wechat/views.py
+++ wechat/views.py
@@ -17,7 +17,7 @@ def weixin_main(request):
         nonce = request.GET.get('nonce', None)
         echostr = request.GET.get('echostr', None)
         #服务器配置中的token
-        token = '写你的token在这里'
+        token = 'lvweicong'
         #把参数放到list中排序后合成一个字符串，再用sha1加密得到新的字符串与微信发来的signature对比，如果相同就返回echostr给服务器，校验通过
         hashlist = [token, timestamp, nonce]
         hashlist.sort()
