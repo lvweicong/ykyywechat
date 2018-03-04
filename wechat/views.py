83270487314cde02b148ab0f59c27bb621a4b373
lvweicong <316421657@qq.com>
2018-03-04 15:42:14 +0800

    Initial commit

--- wechat/views.py
+++ wechat/views.py
@@ -1,7 +1,3 @@
-from django.shortcuts import render
-import hashlib
-import json
-from django.utils.encoding import smart_str
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 from wechat_sdk import WechatBasic
@@ -21,7 +17,6 @@ def weixin_main(request):
     timestamp = request.GET.get('timestamp', None)
     nonce = request.GET.get('nonce', None)
     body_text = request.body
-    #echostr = request.GET.get('echostr', None)
     if wechat.check_signature(signature=signature, timestamp=timestamp, nonce=nonce):
         wechat.parse_data(body_text)
         message = wechat.get_message()
@@ -38,115 +33,3 @@ def weixin_main(request):
             response = wechat.response_text(u'未知')
 
     return HttpResponse(response)
-
-
-
-'''''''''
-    if request.method == "GET":
-        #接收微信服务器get请求发过来的参数
-        print('GET method')
-        signature = request.GET.get('signature', None)
-        timestamp = request.GET.get('timestamp', None)
-        nonce = request.GET.get('nonce', None)
-        echostr = request.GET.get('echostr', None)
-        #服务器配置中的token
-        token = 'lvweicong'
-        #把参数放到list中排序后合成一个字符串，再用sha1加密得到新的字符串与微信发来的signature对比，如果相同就返回echostr给服务器，校验通过
-        hashlist = [token, timestamp, nonce]
-        print(hashlist)
-        hashlist.sort()
-        hashstr = ''.join([s for s in hashlist])
-        hashstr = hashlib.sha1(hashstr.encode('utf-8')).hexdigest()
-        if hashstr == signature:
-          return HttpResponse(echostr)
-        else:
-          return HttpResponse("field")
-    else:
-        othercontent = autoreply(request)
-        return HttpResponse(othercontent)
-
-#微信服务器推送消息是xml的，根据利用ElementTree来解析出的不同xml内容返回不同的回复信息，就实现了基本的自动回复功能了，也可以按照需求用其他的XML解析方法
-import xml.etree.ElementTree as ET
-def autoreply(request):
-    try:
-        webData = request.body
-        xmlData = ET.fromstring(webData)
-
-        print('getpostbody...')
-        msg_type = xmlData.find('MsgType').text
-        ToUserName = xmlData.find('ToUserName').text
-        FromUserName = xmlData.find('FromUserName').text
-        CreateTime = xmlData.find('CreateTime').text
-        MsgType = xmlData.find('MsgType').text
-        MsgId = xmlData.find('MsgId').text
-
-        toUser = FromUserName
-        fromUser = ToUserName
-
-        if msg_type == 'text':
-            content = "您好,欢迎关注永康医院!"
-            replyMsg = TextMsg(toUser, fromUser, content)
-            print ("成功了!!!!!!!!!!!!!!!!!!!")
-            print (replyMsg)
-            return replyMsg.send()
-
-        elif msg_type == 'image':
-            content = "图片已收到,谢谢"
-            replyMsg = TextMsg(toUser, fromUser, content)
-            return replyMsg.send()
-        elif msg_type == 'voice':
-            content = "语音已收到,谢谢"
-            replyMsg = TextMsg(toUser, fromUser, content)
-            return replyMsg.send()
-        elif msg_type == 'video':
-            content = "视频已收到,谢谢"
-            replyMsg = TextMsg(toUser, fromUser, content)
-            return replyMsg.send()
-        elif msg_type == 'shortvideo':
-            content = "小视频已收到,谢谢"
-            replyMsg = TextMsg(toUser, fromUser, content)
-            return replyMsg.send()
-        elif msg_type == 'location':
-            content = "位置已收到,谢谢"
-            replyMsg = TextMsg(toUser, fromUser, content)
-            return replyMsg.send()
-        else:
-            msg_type == 'link'
-            content = "链接已收到,谢谢"
-            replyMsg = TextMsg(toUser, fromUser, content)
-            return replyMsg.send()
-
-    #except Exception, Argment:
-    except (Exception) as Argment:
-        return Argment
-
-class Msg(object):
-    def __init__(self, xmlData):
-        self.ToUserName = xmlData.find('ToUserName').text
-        self.FromUserName = xmlData.find('FromUserName').text
-        self.CreateTime = xmlData.find('CreateTime').text
-        self.MsgType = xmlData.find('MsgType').text
-        self.MsgId = xmlData.find('MsgId').text
-
-import time
-class TextMsg(Msg):
-    def __init__(self, toUserName, fromUserName, content):
-        self.__dict = dict()
-        self.__dict['ToUserName'] = toUserName
-        self.__dict['FromUserName'] = fromUserName
-        self.__dict['CreateTime'] = int(time.time())
-        self.__dict['Content'] = content
-
-    def send(self):
-        XmlForm = """
-        <xml>
-        <ToUserName><![CDATA[{ToUserName}]]></ToUserName>
-        <FromUserName><![CDATA[{FromUserName}]]></FromUserName>
-        <CreateTime>{CreateTime}</CreateTime>
-        <MsgType><![CDATA[text]]></MsgType>
-        <Content><![CDATA[{Content}]]></Content>
-        </xml>
-        """
-        return XmlForm.format(**self.__dict)
-
-'''''''''
